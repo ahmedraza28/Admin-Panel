ed61e6030b68fb3739672841fc182aefda1e811d
Ahmed Raza <ahmedrazasst@gmail.com>
2023-11-23 11:12:08 -0800

    trying to solve reload error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Settings from "./pages/Settings";
@@ -10,14 +10,14 @@ import AdminLogin from "./components/AdminLogin";
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={<Home/>}></Route>
-      <Route exact path="/signin" element={<AdminLogin/>}></Route>
-      <Route exact path="/recipients" element={<Settings/>}></Route>
-      <Route exact path="/account" element={<Account/>}></Route>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter basename="/admin-frontend-2xbk"> {/* Adjust the basename accordingly */}
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/signin" element={<AdminLogin />} />
+          <Route exact path="/recipients" element={<Settings />} />
+          <Route exact path="/account" element={<Account />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
